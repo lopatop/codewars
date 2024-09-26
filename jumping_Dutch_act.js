6febdcbbf7071a62cc564a9f0b4bcfa1c4849ce2
lopatop <pavellopato327@gmail.com>
2024-09-26 18:23:46 +0200

    start

---
+++ jumping_Dutch_act.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Example:
+// sc(2) should return "Aa~ Pa! Aa!"
+// It means:
+// Mr. despair jumped from the 2 floor, the voice is "Aa~"
+// He fell on the ground, the voice is "Pa!"
+// He did not die immediately, and the final voice was "Aa!"
+// sc(6) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Pa! Aa!"
+// sc(7) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+// sc(10) should return "Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Aa~ Pa!"
+// if floor<=1, Mr. despair is safe, return ""
+
+// SOLUTION:
+
+
