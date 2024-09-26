9e9927f1ded244a742c8c97415e212e3a21074c5
lopatop <pavellopato327@gmail.com>
2024-09-26 18:24:54 +0200

    function

--- jumping_Dutch_act.js
+++ jumping_Dutch_act.js
@@ -11,5 +11,14 @@
 // if floor<=1, Mr. despair is safe, return ""
 
 // SOLUTION:
+function sc(floor) {
+    if (floor <= 1) {
+        return ("")
+    } else if (floor < 7) {
+        return "Aa~ ".repeat(floor - 1) + "Pa! " + "Aa!"
+    } else {
+        return "Aa~ ".repeat(floor - 1) + "Pa!"
+    }
+}
 
 
