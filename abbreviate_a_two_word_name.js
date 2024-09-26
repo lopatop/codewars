740d0b4abf32efc7b8766ee839524c1986c94e6c
lopatop <pavellopato327@gmail.com>
2024-09-26 19:13:51 +0200

    function two word name

--- abbreviate_a_two_word_name.js
+++ abbreviate_a_two_word_name.js
@@ -4,3 +4,8 @@
 // patrick feeney => P.F
 
 // SOLUTION: 
+function abbrevName(name){
+    let name1 = name.split(" "); //разбиваем строку на массив слов 
+    let firstLetters = name1[0].charAt(0).toUpperCase() + "." + name1[1].charAt(0).toUpperCase(); //берем первый символ каждого слова и преобразуем в верхний регистр
+    return firstLetters;
+}
