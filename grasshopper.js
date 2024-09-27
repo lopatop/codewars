641e0cfb362c9b3dc34d7e69861c554b3e9d9d5c
lopatop <pavellopato327@gmail.com>
2024-09-27 17:17:33 +0200

    function grasshopper

--- grasshopper.js
+++ grasshopper.js
@@ -2,3 +2,6 @@
 // Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
 
 // SOLUTION:
+function combat(health, damage) {
+    return health > damage ? health = health - damage : health = 0
+} 
