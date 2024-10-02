911a31d96284c7e3f4f64cae0a81d593b177c209
lopatop <pavellopato327@gmail.com>
2024-10-02 18:49:42 +0200

    function min number in arrey

--- smallest_integer_in_the_array.js
+++ smallest_integer_in_the_array.js
@@ -2,3 +2,10 @@
 // Given an array of integers your solution should find the smallest integer.
 
 // SOLUTION: 
+
+function findSmallestInt(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr.sort((a, b) => a - b);
+    }
+    return arr[0];
+}
