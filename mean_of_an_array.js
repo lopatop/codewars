d7e5beca49b0ebd9d24c71b2bfbceaab96f41b93
lopatop <pavellopato327@gmail.com>
2024-10-03 18:30:03 +0200

    function mean of an array

--- mean_of_an_array.js
+++ mean_of_an_array.js
@@ -4,3 +4,12 @@
 // The array will never be empty.
 
 // SOLUTION: 
+
+function getAverage(marks) {
+    let sumArr = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sumArr += marks[i];
+    }
+    meanNumber = sumArr / marks.length;
+    return Math.floor(meanNumber);
+}
