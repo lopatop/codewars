21cb73819b37f73efe9a19229eff80c0ebdadcd7
lopatop <pavellopato327@gmail.com>
2024-10-03 18:29:20 +0200

    first commit

---
+++ mean_of_an_array.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+
+// SOLUTION: 
