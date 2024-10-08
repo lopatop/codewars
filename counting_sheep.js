63cf204ca5738ca84d805211d8dc095184b21a2c
lopatop <pavellopato327@gmail.com>
2024-10-08 23:24:33 +0300

    first commit

---
+++ counting_sheep.js
@@ -0,0 +1,6 @@
+// Description:
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+// Hint: Don't forget to check for bad values like null/undefined
+
+// SOLUTION:
+
