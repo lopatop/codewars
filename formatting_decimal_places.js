cf755d5b1588ca3364fe363e8d22a0d9cccd23af
lopatop <pavellopato327@gmail.com>
2024-10-08 23:15:19 +0300

    first commit

---
+++ formatting_decimal_places.js
@@ -0,0 +1,4 @@
+// Description:
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// SOLUTION:
