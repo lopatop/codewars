1f8f54a0fb18bc653e14aa0c89b2d50711a5be9b
lopatop <pavellopato327@gmail.com>
2024-10-11 22:31:11 +0200

    first commit

---
+++ add_property_to_every_object_in_array.js
@@ -0,0 +1,4 @@
+// Description:
+// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+// SOLUTION:
