d36467b14db3e3ff96ae3c3c05b0b4a825a83d0b
lopatop <pavellopato327@gmail.com>
2024-10-11 22:29:20 +0200

    function anima

--- basic_data_types_object.js
+++ basic_data_types_object.js
@@ -2,3 +2,7 @@
 // In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
 
 // SOLUTION:
+
+function animal(obj) {
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
