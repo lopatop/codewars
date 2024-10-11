a30421bc3151144546a3bb885544aad9b08704ae
lopatop <pavellopato327@gmail.com>
2024-10-11 22:31:56 +0200

    function add property

--- add_property_to_every_object_in_array.js
+++ add_property_to_every_object_in_array.js
@@ -2,3 +2,7 @@
 // Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
 
 // SOLUTION:
+
+for (let i = 0; i < questions.length; i++) {
+    questions[i].usersAnswer = null;
+}
