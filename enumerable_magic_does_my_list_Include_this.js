057424746ae0691465edd7e8b88c54d5f6871ba7
lopatop <pavellopato327@gmail.com>
2024-10-12 20:09:12 +0200

    function include

--- enumerable_magic_does_my_list_Include_this.js
+++ enumerable_magic_does_my_list_Include_this.js
@@ -3,3 +3,11 @@
 
 // SOLUTION:
 
+function include(arr, item) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return true;
+        }
+    }
+    return false;
+}
