425ffed17f5c148a0437273eef49a6e08af4b520
lopatop <pavellopato327@gmail.com>
2024-10-12 20:06:16 +0200

    function add property

--- add_property_to_an_object.js
+++ add_property_to_an_object.js
@@ -2,3 +2,13 @@
 // Write a function that adds a named property to an object. It must be possible to set the property to a new value. If the property already exists on the object, and error should be thrown.
 
 // SOLUTION:
+
+function addProperty(obj, prop, value) {
+    if (prop in obj) {
+        throw new Error('Error message');
+
+    } else {
+        obj[prop] = value;
+
+    }
+}
