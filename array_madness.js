e06de557b3bb5c2d1f1d96b9e78bb90accc8aa5b
lopatop <pavellopato327@gmail.com>
2024-10-14 18:43:59 +0200

    function array madness

--- array_madness.js
+++ array_madness.js
@@ -5,3 +5,15 @@
 // arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
 
 // SOLUTION:
+
+function arrayMadness(a, b) {
+    let arr1 = 0;
+    let arr2 = 0;
+    for (let i = 0; i < a.length; i++) {
+        arr1 += a[i] ** 2;
+    }
+    for (let i = 0; i < b.length; i++) {
+        arr2 += b[i] ** 3;
+    }
+    return arr1 > arr2;
+}
