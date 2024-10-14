368961a8a5ee3449f3b1f6610f71a02ae31ac2e8
lopatop <pavellopato327@gmail.com>
2024-10-14 18:46:32 +0200

    function calculator

--- simple_calculator.js
+++ simple_calculator.js
@@ -10,3 +10,19 @@
 // calculator(1,"k","*"); //=> result will be "unknown value"
 
 // SOLUTION:
+
+function calculator(a, b, sign) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        return "unknown value";
+    } else if (sign == "+") {
+        return a + b;
+    } else if (sign == "-") {
+        return a - b;
+    } else if (sign == "*") {
+        return a * b;
+    } else if (sign == "/") {
+        return a / b;
+    } else {
+        return "unknown value";
+    }
+}
