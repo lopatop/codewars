9c08f2da3ce6b6412b74eeceadbfe969ba1f8d1f
lopatop <pavellopato327@gmail.com>
2024-10-15 20:11:48 +0200

    first commit

---
+++ higher_order_functions_series.js
@@ -0,0 +1,7 @@
+// Description:
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+// Hi < firstName here >, what do you like the most about < language here >?
+// For example, given the following input array:
+
+// DESCRIPTION:
