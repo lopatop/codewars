9d12fa2d8aeb68eb4c642ae3fa2bd968c795fb3d
lopatop <pavellopato327@gmail.com>
2024-10-15 20:07:03 +0200

    function define card

--- define_card_suit.js
+++ define_card_suit.js
@@ -7,3 +7,15 @@
 // ('3♠') -> return 'spades'
 
 // SOLUTION:
+
+function defineSuit(card) {
+    if (card.includes('♣')) {
+        return 'clubs';
+    } else if (card.includes('♦')) {
+        return 'diamonds';
+    } else if (card.includes('♥')) {
+        return 'hearts';
+    } else if (card.includes('♠')) {
+        return 'spades';
+    }
+}
