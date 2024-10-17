685ecf4a4f689747143c1e040ffeabeac9fb90c6
lopatop <pavellopato327@gmail.com>
2024-10-17 23:24:28 +0200

    first commit

---
+++ who's_pnline.js
@@ -0,0 +1,5 @@
+// Description:
+// You have a group chat application, but who is online!?
+// You want to show your users which of their friends are online and available to chat
+// If someone is online but their lastActivity was more than 10 minutes ago they are to be considered away.
+
