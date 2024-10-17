2df74955d10d757f7c7b6d042370b5c5a4cbc00c
lopatop <pavellopato327@gmail.com>
2024-10-17 23:25:39 +0200

    input data

--- who's_pnline.js
+++ who's_pnline.js
@@ -3,3 +3,33 @@
 // You want to show your users which of their friends are online and available to chat
 // If someone is online but their lastActivity was more than 10 minutes ago they are to be considered away.
 
+// The input data has the following structure:
+// [{
+//   username: 'David',
+//   status: 'online',
+//   lastActivity: 10
+// }, {
+//   username: 'Lucy', 
+//   status: 'offline',
+//   lastActivity: 22
+// }, {
+//   username: 'Bob', 
+//   status: 'online',
+//   lastActivity: 104
+// }]
+// The corresponding output should look as follows:
+// {
+//   online: ['David'],
+//   offline: ['Lucy'],
+//   away: ['Bob']
+// }
+// If for example, no users are online the output should look as follows:
+// {
+//   offline: ['Lucy'],
+//   away: ['Bob']
+// }
+// username will always be a string, status will always be either 'online' or 'offline' (UserStatus enum in C#) and lastActivity will always be number >= 0.
+// Finally, if you have no friends in your chat application, the input will be an empty array []. In this case you should return an empty object {} (empty Dictionary in C#).
+
+
+// SOLUTION:
