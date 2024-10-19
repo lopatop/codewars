5f8af933202afe5f7e9fe42775a995c4718d3275
lopatop <pavellopato327@gmail.com>
2024-10-19 22:34:14 +0200

    function basicOp

--- basic_mathematical_operations.js
+++ basic_mathematical_operations.js
@@ -10,3 +10,17 @@
 
 
 // SOLUTION:
+
+function basicOp(operation, value1, value2) {
+    let result;
+    if (operation === '+') {
+        result = value1 + value2;
+    } else if (operation === '-') {
+        result = value1 - value2;
+    } else if (operation === '*') {
+        result = value1 * value2;
+    } else if (operation === '/') {
+        result = value1 / value2;
+    }
+    return result
+}
