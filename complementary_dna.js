5c75a8b7e292dbde8f9ce036ba1df8b166ad41d7
lopatop <pavellopato327@gmail.com>
2024-10-19 22:41:49 +0200

    function dnaStarand

--- complementary_dna.js
+++ complementary_dna.js
@@ -8,3 +8,19 @@
 // "GTAT" --> "CATA"
 
 // SOLUTION:
+
+function dnaStrand(dna) {
+    let str = ""
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] === "A") {
+            str += "T";
+        } else if (dna[i] === "T") {
+            str += "A";
+        } else if (dna[i] === "C") {
+            str += "G";
+        } else if (dna[i] === "G") {
+            str += "C";
+        }
+    }
+    return str;
+}
