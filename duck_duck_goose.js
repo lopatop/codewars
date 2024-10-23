cd32e2f881550880286c8a3597519abc5787f43c
lopatop <pavellopato327@gmail.com>
2024-10-23 21:06:17 +0200

    example

--- duck_duck_goose.js
+++ duck_duck_goose.js
@@ -3,3 +3,8 @@
 // Task:
 // Given an array of Player objects and a position (first position is 1), return the name of the chosen Player.
 // name is a property of Player objects, e.g Player.name
+
+// Example:
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.nam
