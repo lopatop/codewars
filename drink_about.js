17ec3737be6da9d858ec375bd9e891aa0b732918
lopatop <pavellopato327@gmail.com>
2024-10-23 21:31:51 +0200

    function peopleWithAgeDrink

--- drink_about.js
+++ drink_about.js
@@ -17,3 +17,15 @@
 // 18 --> "drink beer"
 // 20 --> "drink beer"
 // 30 --> "drink whisky"
+
+function peopleWithAgeDrink(old) {
+    if (old < 14) {
+        return "drink toddy"
+    } else if (old < 18) {
+        return "drink coke"
+    } else if (old < 21) {
+        return "drink beer"
+    } else {
+        return "drink whisky"
+    }
+}
