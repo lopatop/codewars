2412f336da8f26bc5485d39475aa6bac5291dbb9
lopatop <pavellopato327@gmail.com>
2024-10-24 19:40:15 +0200

    function correctPolishLetters

--- polish_alphabet.js
+++ polish_alphabet.js
@@ -15,3 +15,28 @@
 // "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
 
 // SOLUTION:
+
+function correctPolishLetters(string) {
+    let strPolish = '';
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "ą") {
+            strPolish += "a"
+        } else if (string[i] === "ć") {
+            strPolish += "c"
+        } else if (string[i] === "ę") {
+            strPolish += "e"
+        } else if (string[i] === "ł") {
+            strPolish += "l"
+        } else if (string[i] === "ń") {
+            strPolish += "n"
+        } else if (string[i] === "ó") {
+            strPolish += "o"
+        } else if (string[i] === "ś") {
+            strPolish += "s"
+        } else if (string[i] === "ź") {
+            strPolish += "z"
+        } else if (string[i] === "ż") {
+            strPolish += "z"
+        } else { strPolish += string[i] }
+    } return strPolish;
+}
