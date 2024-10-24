316b10c6ed42e46ad79ea1af872f1af187fa5826
lopatop <pavellopato327@gmail.com>
2024-10-24 19:45:29 +0200

    function areYouPlayingBanjo

--- are_you_playing_banjo.js
+++ are_you_playing_banjo.js
@@ -8,5 +8,11 @@
 
 // SOLUTION:
 
-
+function areYouPlayingBanjo(name) {
+    if (name[0] === "R" || name[0] === "r") {
+        return `${name} plays banjo`
+    } else {
+        return `${name} does not play banjo`
+    }
+}
 
