887a7ba5b8a1bee6342654f72fda379f3cd8fae8
lopatop <pavellopato327@gmail.com>
2024-10-26 17:10:55 +0200

    function gooseFilter

--- filter_out_the_geese.js
+++ filter_out_the_geese.js
@@ -9,3 +9,8 @@
 // The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 
 // SOLUTION:
+
+export function gooseFilter(birds: string[]): string[] {
+    const geese: string[] = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(bird => !geese.includes(bird));
+}
