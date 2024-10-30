bf0214b3f24bf70eeb53e929bb6b4720a6149534
lopatop <pavellopato327@gmail.com>
2024-10-30 21:47:18 +0100

    function cutIt

--- slice_methods_of_string.js
+++ slice_methods_of_string.js
@@ -9,3 +9,9 @@
 // cutIt(["codewars","javascript","java"]) should return ["code","java","java"]
 
 // SOLUTION:
+
+function cutIt(arr) {
+    const minLength = Math.min(...arr.map(str => str.length));
+    return arr.map(str => str.slice(0, minLength))
+
+}
