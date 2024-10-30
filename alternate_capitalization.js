5117bd790b30a2a33b5c305a3bd59278e6b34724
lopatop <pavellopato327@gmail.com>
2024-10-30 21:51:13 +0100

    function capitalize

--- alternate_capitalization.js
+++ alternate_capitalization.js
@@ -3,3 +3,24 @@
 // For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
 
 // SOLUTION:
+
+function capitalize(s) {
+    let result = [];
+    const arrS = s.split('');
+    console.log(arrS);
+
+    const index0 = arrS.map((el, index) =>
+        index % 2 !== 1 ? el.toUpperCase() : el
+    )
+    const arr0 = index0.join('');
+    result.push(arr0);
+
+
+    console.log(index0.join('').split());
+    const index1 = arrS.map((el, index) =>
+        index % 2 === 1 ? el.toUpperCase() : el
+    )
+    const arr1 = index1.join('');
+    result.push(arr1);
+    return result;
+};
