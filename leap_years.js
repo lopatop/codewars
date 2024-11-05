46b1d16f94975a40b5c78a59f5ec12e7ca929a70
lopatop <pavellopato327@gmail.com>
2024-11-05 18:39:05 +0100

    function isLEapYear

--- leap_years.js
+++ leap_years.js
@@ -7,3 +7,13 @@
 
 // SOLUTION:
 
+function isLeapYear(year) {
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    } else if
+        (year % 4 === 0 || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
