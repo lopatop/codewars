f4d41fdf7ef304ce62fac21ae56045cb1b2f1e0e
lopatop <pavellopato327@gmail.com>
2024-11-07 20:27:25 +0100

    function reverseSeq

--- reversed_sequence.js
+++ reversed_sequence.js
@@ -3,3 +3,13 @@
 // Example : n=5 --> [5,4,3,2,1]
 
 // SOLUTION:
+
+const reverseSeq = n => {
+    let array = []
+    for (let i = n; i > 0; i--) {
+
+        array.push(i);
+    }
+
+    return array;
+};
