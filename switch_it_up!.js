a153baadc4bf8cad81466029c406b38d943dd277
lopatop <pavellopato327@gmail.com>
2024-11-08 22:04:24 +0100

    first commit

---
+++ switch_it_up!.js
@@ -0,0 +1,7 @@
+// Description:
+// When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+// Input: 1
+// Output: "One".
+
+// SOLUTION: 
+
