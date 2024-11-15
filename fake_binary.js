ebd30a158b621c672fe829a99c34dae9c602599d
lopatop <pavellopato327@gmail.com>
2024-11-15 20:09:37 +0100

    function fakeBin

--- fake_binary.js
+++ fake_binary.js
@@ -4,3 +4,14 @@
 
 // SOLUTION:
 
+function fakeBin(x) {
+    let result = ''
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] > 4) {
+            result += 1
+        } else {
+            result += 0
+        }
+    }
+    return result
+}
