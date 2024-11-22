4514327a5f8ec6edcdd7de232b25613bc5387ad9
lopatop <pavellopato327@gmail.com>
2024-11-22 17:52:19 +0100

    first commit

---
+++ ones_complement.js
@@ -0,0 +1,10 @@
+// The Ones' Complement of a binary number is the number obtained by swapping all the 0s for 1s and all the 1s for 0s.
+// For any given binary number,formatted as a string, return the Ones' Complement of that number.
+// Examples
+// "0"    -> "1"
+// "1"    -> "0"
+// "000"  -> "111"
+// "1001" -> "0110"
+// "1001" -> "0110"
+
+// SOLUTION: 
