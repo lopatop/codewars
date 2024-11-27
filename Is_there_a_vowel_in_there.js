3e2aecf9289d3f71f9246bba372c647fa1c2e890
lopatop <pavellopato327@gmail.com>
2024-11-27 21:07:36 +0100

    function isVow

--- Is_there_a_vowel_in_there.js
+++ Is_there_a_vowel_in_there.js
@@ -4,3 +4,19 @@
 // Return the resulting array.
 
 // SOLUTION:
+
+function isVow(a) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === 97) {
+            a[i] = 'a'
+        } else if (a[i] === 101) {
+            a[i] = 'e'
+        } else if (a[i] === 105) {
+            a[i] = 'i'
+        } else if (a[i] === 111) {
+            a[i] = 'o'
+        } else if (a[i] === 117) {
+            a[i] = 'u'
+        }
+    } return a
+}
