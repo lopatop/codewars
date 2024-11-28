ecda8aad4c2a147cbb787c61449dc3e30f4ceec3
lopatop <pavellopato327@gmail.com>
2024-11-28 21:16:13 +0100

    function createArray

--- unfinished_loop_bug_fixing.js
+++ unfinished_loop_bug_fixing.js
@@ -3,3 +3,11 @@
 // Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
 
 // SOLUTION: 
+
+function createArray(number) {
+    var newArray = [];
+    for (var counter = 1; counter <= number; counter++) {
+        newArray.push(counter);
+    }
+    return newArray;
+}
