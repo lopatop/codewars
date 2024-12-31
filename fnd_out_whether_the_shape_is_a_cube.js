3e5f2106792b80b91ae224a3af044b2b9ebe981b
lopatop <pavellopato327@gmail.com>
2024-12-31 20:59:08 +0100

    function cubeChecker

--- fnd_out_whether_the_shape_is_a_cube.js
+++ fnd_out_whether_the_shape_is_a_cube.js
@@ -14,3 +14,8 @@
 // Note: side will be an integer
 
 // SOLUTION:
+
+function cubeChecker(volume, side) {
+    if (volume <= 0 || side <= 0) return false;
+    return (volume / side) / side === side ? true : false
+};
