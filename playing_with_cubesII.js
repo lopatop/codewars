c5ddc9eeaaa9eafbf9c85b6ffd163cea7b967fb2
lopatop <pavellopato327@gmail.com>
2025-01-19 22:41:24 +0100

    first commit

---
+++ playing_with_cubesII.js
@@ -0,0 +1,7 @@
+// Description:
+//     Hey Codewarrior!
+//     In the previous kata, you have implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code one (or more) such that one can instantiate an object via it, handling either no arguments or a single integer.
+// Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+//     The constructor taking no arguments should assign 0 to Cube's Side property.
+
+// SOLUTION:
