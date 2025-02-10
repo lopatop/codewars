eab87d800d18f89774b3a2b48dbb9626a76021a4
lopatop <pavellopato327@gmail.com>
2025-02-10 22:37:18 +0100

    function getMinMax

--- find_min_and_max.js
+++ find_min_and_max.js
@@ -2,3 +2,11 @@
 //     Implement a function that returns the minimal and the maximal value of a list (in this order).
 
 // SOLUTION:
+
+function getMinMax(arr){
+    if(arr.length>1){
+        const sortArr = arr.sort((a,b)=>a-b)
+        return [sortArr[0], sortArr[arr.length-1]]
+    }
+    return [arr[0],arr[0]]
+}
