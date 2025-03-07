488425dc9a27aceb318ea338820511b8943ed8d8
lopatop <pavellopato327@gmail.com>
2025-03-07 23:19:56 +0100

    function getSize

--- surface_area_and_volume_of_a_box.js
+++ surface_area_and_volume_of_a_box.js
@@ -6,3 +6,9 @@
 //     The output will be language dependant, so please check sample tests for the corresponding data type, (list, tuple, struct, query, et cetera).
 
 // SOLUTION:
+
+function getSize(width, height, depth){
+    let s = 2*(width*height+width*depth+height*depth)
+    let v = width*height*depth
+    return [s,v]
+}
