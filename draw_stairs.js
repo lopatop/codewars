f2606aa91a121295648ab7225ac394f5e32bdc65
lopatop <pavellopato327@gmail.com>
2025-03-16 18:33:01 +0100

    function drawStairs(

--- draw_stairs.js
+++ draw_stairs.js
@@ -18,3 +18,11 @@
 
 // SOLUTION:
 
+function drawStairs(n) {
+    let result = ''
+    for (let i=0; i<n; i++) {
+        result+= ' '.repeat(i)+'I'
+        if(i<n-1) result+='\n'
+    }return result
+}
+
