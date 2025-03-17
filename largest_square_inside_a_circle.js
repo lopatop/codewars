9071a8b0190156f746971e70dc2f840089f7ca5d
lopatop <pavellopato327@gmail.com>
2025-03-17 18:15:05 +0100

    function areaLargestSquare

--- largest_square_inside_a_circle.js
+++
@@ -1,8 +0,0 @@
-// Description:
-//     Determine the area of the largest square that can fit inside a circle with radius r.
-//
-// SOLUTION:
-
-function areaLargestSquare(r) {
-    return r**2*2
-}
