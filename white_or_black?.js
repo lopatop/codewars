8765b96861a601346e5b0e387fc9b170138bf63c
lopatop <pavellopato327@gmail.com>
2025-05-04 23:23:43 +0200

    first commit

---
+++ white_or_black?.js
@@ -0,0 +1,13 @@
+// Description:
+//     Complete the function that returns the color of the given square on a normal, 8x8 chess board:
+//
+//     chessboard
+//
+// Examples
+// 'a', 8  ==>  "white"
+// 'b', 2  ==>  "black"
+// 'f', 5  ==>  "white"
+
+
+// SOLUTION:
+
