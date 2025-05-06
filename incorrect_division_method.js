269d11bb44754726b2b2c80f806192a9406aae3c
lopatop <pavellopato327@gmail.com>
2025-05-06 22:22:36 +0200

    solve()

--- incorrect_division_method.js
+++ incorrect_division_method.js
@@ -2,3 +2,6 @@
 //     This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
 
 // SOLUTION:
+
+
+const solve = (x, y) => x/y
