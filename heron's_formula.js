65f9bbac8be09c29c937ddb04ea566f6b2d145da
lopatop <pavellopato327@gmail.com>
2025-06-05 23:37:47 +0200

    first commit

---
+++ heron's_formula.js
@@ -0,0 +1,9 @@
+// Description:
+//     Write function heron which calculates the area of a triangle with sides a, b, and c.
+//
+//
+// Notes
+// All inputs are valid triangles with integer sides.
+//     You do not need to round anything. Answers will be tested for correctness within a margin of 0.01.
+
+// SOLUTION:
