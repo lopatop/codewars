6c332ff913575b6d98fd05d39b7d329bff08b2ad
lopatop <pavellopato327@gmail.com>
2025-06-05 23:38:13 +0200

    function heron

--- heron's_formula.js
+++ heron's_formula.js
@@ -7,3 +7,9 @@
 //     You do not need to round anything. Answers will be tested for correctness within a margin of 0.01.
 
 // SOLUTION:
+
+
+function heron(a, b, c) {
+    const s = (a+b+c)/2
+    return Math.sqrt(s*(s-a)*(s-b)*(s-c))
+}
