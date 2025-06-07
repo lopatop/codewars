26917c0ab025447ecd4dffc68c7f94bd55b2c641
lopatop <pavellopato327@gmail.com>
2025-06-07 23:49:58 +0200

    function shiftLeft

--- shift_left.js
+++ shift_left.js
@@ -12,3 +12,17 @@
 //     If the string is already empty, you cannot perform any more delete operations.
 
 // SOLUTION:
+
+function shiftLeft(s, t) {
+    let i = s.length - 1;
+    let j = t.length - 1;
+    let common = 0;
+
+    while (i >= 0 && j >= 0 && s[i] === t[j]) {
+        common++;
+        i--;
+        j--;
+    }
+
+    return (s.length - common) + (t.length - common);
+}
