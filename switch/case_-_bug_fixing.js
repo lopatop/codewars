a073b5177348af25fdcdc1490780a1405da02ee6
lopatop <pavellopato327@gmail.com>
2025-06-08 20:05:02 +0200

    function evalObject

--- switch/case_-_bug_fixing.js
+++ switch/case_-_bug_fixing.js
@@ -3,3 +3,16 @@
 // Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
 
 // SOLUTION:
+
+function evalObject(value){
+    var result = 0;
+    switch(value.operation){
+        case'+':  return result = value.a + value.b;
+        case'-': return result = value.a - value.b;
+        case'/': return result = value.a / value.b;
+        case'*':  return result = value.a * value.b;
+        case'%': return result = value.a % value.b;
+        case'^': return result = Math.pow(value.a, value.b);
+    }
+    return result;
+}
