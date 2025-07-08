fec61ea639abddc96c0cef9dcc38feefef2b81ae
lopatop <pavellopato327@gmail.com>
2025-07-08 20:36:44 +0200

    greet()

--- function_1_hello_world.js
+++ function_1_hello_world.js
@@ -5,3 +5,5 @@
 // Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
 
 //SOLUTION
+
+const greet = () => 'hello world!'
