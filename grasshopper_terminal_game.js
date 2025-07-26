18eb981cd126c334d8720a4c9bf77c70dc641ad4
lopatop <pavellopato327@gmail.com>
2025-07-26 20:55:16 +0200

    class Hero

--- grasshopper_terminal_game.js
+++ grasshopper_terminal_game.js
@@ -11,3 +11,12 @@
 
 //SOLUTION
 
+class Hero {
+    constructor(name = 'Hero', position = '00', health = 100, damage = 5, experience = 0) {
+        this.name = name;
+        this.position = position;
+        this.health = health;
+        this.damage = damage;
+        this.experience = experience;
+    }
+}
