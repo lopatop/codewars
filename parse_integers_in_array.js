74469d6098a4d6df0f8aec7bae8aedb001088d1b
lopatop <pavellopato327@gmail.com>
2025-12-07 23:52:08 +0100

    fist commit

---
+++ parse_integers_in_array.js
@@ -0,0 +1,9 @@
+// Description:
+//     A colleague asked if you can help him fix his function. It seems it doesn't always parse Integers correctly.
+//
+// Examples:
+//
+//     parseNumbers(['10']) // should return [10]
+// parseNumbers(['-1','0','1']) // should return [-1,0,1]
+
+// SOLUTION:
