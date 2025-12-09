3358d77710f29210384e849fa1f2ec58054bc271
lopatop <pavellopato327@gmail.com>
2025-12-09 23:39:41 +0100

    first commit

---
+++ sum_of_a_nested_list.js
@@ -0,0 +1,7 @@
+// Description:
+//     Implement a function to calculate the sum of the numerical values in a nested list. For example :
+//
+//     sumNested([1, [2, [3, [4]]]]) => 10
+
+//SOLUTION
+
