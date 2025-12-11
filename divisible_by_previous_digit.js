59e888d10cf4ea2c40c4977deeb68c16d4767a02
lopatop <pavellopato327@gmail.com>
2025-12-11 21:51:00 +0100

    function divisibleByLast

--- divisible_by_previous_digit.js
+++ divisible_by_previous_digit.js
@@ -10,3 +10,22 @@
 // *** Remember 0 is evenly divisible by all integers but not the other way around ***
 
 //SOLUTION
+
+function divisibleByLast(n) {
+    const arr = n.toString().split('').map(Number)
+    const res = []
+
+    for(let i = 0; i < arr.length; i++){
+        const prev = arr[i-1]
+        const curr = arr[i]
+
+        if(prev === 0){
+            res.push(false)
+        }else{
+            res.push(curr%prev === 0)
+        }
+    }
+
+    return res
+
+}
