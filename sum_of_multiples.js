1692d6e3bc2517e1fc8b4a10bfd69a8c951470fe
lopatop <pavellopato327@gmail.com>
2025-12-26 22:42:34 +0100

    first commit

---
+++ sum_of_multiples.js
@@ -0,0 +1,14 @@
+// Description:
+//     Your Job
+// Find the sum of all multiples of n below m
+//
+// Keep in Mind
+// n and m should be natural numbers (positive integers). Otherwise, see the examples in your language about how to handle invalid input values.
+//     m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+//SOLUTION
