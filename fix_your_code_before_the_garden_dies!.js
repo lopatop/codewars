1c24b8faa8d7758be87cb4595e8d74dc728521f9
lopatop <pavellopato327@gmail.com>
2025-12-29 23:57:08 +0100

    function rainAmount

--- fix_your_code_before_the_garden_dies!.js
+++ fix_your_code_before_the_garden_dies!.js
@@ -5,3 +5,16 @@
 
 //SOLUTION
 
+function rainAmount(mm){
+//     if (rainAmount = 40) {
+//          return "You need to give your plant " + {rainAmount - 40} + " mm of water"
+//     };
+//     else {
+//          return "Your plant has had more than enough water for today!"
+//     };
+
+    return mm >=40? "Your plant has had more than enough water for today!"
+        : `You need to give your plant ${40 - mm}mm of water`
+
+
+}
