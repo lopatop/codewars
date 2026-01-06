3729d24c8c9ad2f80952a42d7d4626a65b9f81a6
lopatop <pavellopato327@gmail.com>
2026-01-06 23:42:03 +0100

    function dative

--- hungarian_vowe_harmony.js
+++ hungarian_vowe_harmony.js
@@ -17,3 +17,25 @@
 //     To keep it simple: All words end with a consonant :)
 // All strings are unicode strings.
 //     There are no grammatical exceptions in the tests.
+
+//SOLUTION
+
+function dative(word) {
+    const backVowels = ['a', 'á', 'o', 'ó', 'u', 'ú'];
+    const frontVowels = ['e', 'é', 'i', 'í', 'ö', 'ő', 'ü', 'ű'];
+
+    let suffix = '';
+
+    for (let i = word.length - 1; i >= 0; i--) {
+        if (backVowels.includes(word[i])) {
+            suffix = 'nak';
+            break;
+        }
+        if (frontVowels.includes(word[i])) {
+            suffix = 'nek';
+            break;
+        }
+    }
+
+    return word + suffix;
+}
