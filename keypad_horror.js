5a713dd281a94e79cf4723c5c80fb04928d851ef
lopatop <pavellopato327@gmail.com>
2026-01-08 21:59:05 +0100

    function computerToPhone

--- keypad_horror.js
+++ keypad_horror.js
@@ -21,3 +21,20 @@
 //     You get a string with numbers only
 
 //SOLUTION
+
+function computerToPhone(numbers) {
+    let res = '';
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] == 0) res += 0
+        if(numbers[i] == 1) res += 7
+        if(numbers[i] == 2) res += 8
+        if(numbers[i] == 3) res += 9
+        if(numbers[i] == 4) res += 4
+        if(numbers[i] == 5) res += 5
+        if(numbers[i] == 6) res += 6
+        if(numbers[i] == 7) res += 1
+        if(numbers[i] == 8) res += 2
+        if(numbers[i] == 9) res += 3
+    }
+    return res
+}
