da03615b6c24baefda520409655d6380769a162f
lopatop <pavellopato327@gmail.com>
2026-01-10 23:37:47 +0100

    first commit

---
+++ js_golfing_undefined.js
@@ -0,0 +1,14 @@
+// Description:
+//
+//     JS Golfing Series
+//
+// Task:
+//
+//     Return undefined with no more than 10 characters.
+//
+//     Digits and letters are not allowed!
+//     Your function should take exactly `0` parameters.
+
+//SOLUTION
+
+
