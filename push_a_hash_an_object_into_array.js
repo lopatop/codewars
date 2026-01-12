043130e794cc8e73b9df7aeb1c901d4063eae51f
lopatop <pavellopato327@gmail.com>
2026-01-12 23:54:41 +0100

    first commit

---
+++ push_a_hash_an_object_into_array.js
@@ -0,0 +1,5 @@
+// Description:
+//     You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+//SOLUTION
+
