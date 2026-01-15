cbafdc6ef8344803ef89120c6f0c7f4f5483b848
lopatop <pavellopato327@gmail.com>
2026-01-15 22:57:30 +0100

    consecutive()

--- consecutive_items.js
+++ consecutive_items.js
@@ -5,3 +5,10 @@
 
 //SOLUTION
 
+function consecutive(arr, a, b) {
+    for(let i = 0; i < arr.length; i++){
+        if ((arr[i] == a && arr[i+1] == b) || (arr[i] == b && arr[i+1] == a)) return true
+
+    }
+    return false
+}
