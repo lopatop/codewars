8dc584718b2c7bd319a4445c56baa61ae094deac
lopatop <pavellopato327@gmail.com>
2026-02-01 23:52:07 +0100

    twiceAsOld()

--- twice_as_old.js
+++ twice_as_old.js
@@ -6,3 +6,8 @@
 // Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
 
 //SOLUTION
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    const res = sonYearsOld*2 - dadYearsOld
+    return res <0? -1*res: res
+}
