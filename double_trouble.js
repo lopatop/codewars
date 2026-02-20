4d60b8f5d54fd50c7f572bc3e2f35339b79aca8a
lopatop <pavellopato327@gmail.com>
2026-02-20 22:21:52 +0100

    trouble()

--- double_trouble.js
+++ double_trouble.js
@@ -15,3 +15,13 @@
 //     Return the resulting array.
 
 //SOLUTION
+
+function trouble(x, t){
+    for(let i = 0; i < x.length;i++){
+        if(x[i]+x[i+1] === t){
+            x.splice(i+1,1)
+            i--
+        }
+    }
+    return x
+}
