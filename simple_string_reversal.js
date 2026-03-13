5681e94f9a238cfd32c1f1bca82b95515627ad4f
lopatop <pavellopato327@gmail.com>
2026-03-13 22:03:43 +0100

    first commit

---
+++ simple_string_reversal.js
@@ -0,0 +1,11 @@
+// Description:
+//     In this Kata, you will be given a string and two indexes (a and b). Your task is to reverse the portion of that string between those two indices inclusive.
+//
+//     str = "codewars", a = 1, b = 5 --> "cawedors"
+// str = "cODEWArs", a = 1, b = 5 --> "cAWEDOrs"
+// Input will be lowercase and uppercase letters only.
+//
+//     The first index a will always be smaller than the string length; the second index b can be greater than the string length. More examples in the test cases. Good luck!
+
+//SOLUTION
+
