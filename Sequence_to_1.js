0bd3091f24693e07644f3208bf8311b65f84bc13
lopatop <pavellopato327@gmail.com>
2026-03-17 23:33:25 +0100

    first commit

---
+++ Sequence_to_1.js
@@ -0,0 +1,15 @@
+// Description:
+//     Task
+// Given the number n, return the sequence of numbers from n to 1.
+//
+//
+// Range
+// The number n can be negative and also large number:  -9999  <=  n  <=  9999
+//
+//
+// Examples
+// n =  5    >>     5, 4, 3, 2, 1
+// n = -1    >>    -1, 0, 1
+
+//SOLUTION
+
