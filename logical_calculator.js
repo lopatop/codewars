cbc3e901ebec0a12441cec001152b30cc8dc77dc
lopatop <pavellopato327@gmail.com>
2026-04-10 23:24:57 +0200

    logicalCalc()

--- logical_calculator.js
+++ logical_calculator.js
@@ -25,3 +25,16 @@
 
 //SOLUTION
 
+function logicalCalc(array, op) {
+    if (op === 'AND') {
+        return array.reduce((acc, el) => acc && el)
+    }
+
+    if (op === 'OR') {
+        return array.reduce((acc, el) => acc || el)
+    }
+
+    if (op === 'XOR') {
+        return array.reduce((acc, el) => acc !== el)
+    }
+}
