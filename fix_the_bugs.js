3630a2a0ae6586beecbe8ccbd6140bf60a0f5779
lopatop <pavellopato327@gmail.com>
2026-05-25 19:49:32 +0200

    myFirstKata()

--- fix_the_bugs.js
+++ fix_the_bugs.js
@@ -22,3 +22,10 @@
 
 //SOLUTION
 
+const myFirstKata=(a, b)=> {
+    if (typeof(a) !== "number" || typeof(b) !== "number") {
+        return false
+    } else {
+        return (a%b)+(b%a)
+    }
+}
