80c5a9bdfa6444c9c12c5f624621cb8a3bfcca30
lopatop <pavellopato327@gmail.com>
2026-06-10 22:28:09 +0200

    caffeineBuzz()

--- caffeine_script.js
+++ caffeine_script.js
@@ -16,3 +16,10 @@
 // 12  -->  "CoffeeScript"
 
 //SOLUTION
+
+function caffeineBuzz(n) {
+    if(n % 3 === 0 && n % 4 === 0 && n % 2 === 0) return "CoffeeScript"
+    if(n % 3 === 0 && n % 2 === 0) return "JavaScript"
+    if(n % 3 === 0) return "Java"
+    return  "mocha_missing!"
+}
