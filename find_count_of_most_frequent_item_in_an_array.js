1acbbe44f3f05e078cac2607736524700036e17e
lopatop <pavellopato327@gmail.com>
2026-06-17 21:33:18 +0200

    mostFrequentItemCount()

--- find_count_of_most_frequent_item_in_an_array.js
+++ find_count_of_most_frequent_item_in_an_array.js
@@ -8,3 +8,17 @@
 
 //SOLUTION
 
+function mostFrequentItemCount(collection) {
+    if(collection.length === 0) return 0
+    const counts = {}
+    let maxCount = 0
+
+    for(const item of collection){
+        counts[item] = (counts[item] || 0) + 1
+
+        if(counts[item] > maxCount){
+            maxCount = counts[item]
+        }
+    }
+    return maxCount
+}
