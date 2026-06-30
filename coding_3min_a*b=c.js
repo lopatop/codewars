e7948c0ed8496bffd22a383835af233869f2ef1f
lopatop <pavellopato327@gmail.com>
2026-06-30 21:43:44 +0200

    findAB()

--- coding_3min_a*b=c.js
+++ coding_3min_a*b=c.js
@@ -15,3 +15,14 @@
 
 
 //SOLUTION
+
+function findAB(numbers,c){
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = i + 1; j < numbers.length; j++){
+            if( numbers[i] * numbers[j] === c){
+                return [numbers[i],numbers[j]]
+            }
+        }
+    }
+    return null
+}
